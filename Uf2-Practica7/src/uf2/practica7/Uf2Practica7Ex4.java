e9ab74d66d1e4d76d64d930a6af4a5c33613742b
ausias <ausias@hINFO6-8.xiam.cat>
2022-12-20 12:55:22 +0100

    ex4

--- Uf2-Practica7/src/uf2/practica7/Uf2Practica7Ex4.java
+++ Uf2-Practica7/src/uf2/practica7/Uf2Practica7Ex4.java
@@ -12,19 +12,37 @@ public class Uf2Practica7Ex4 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        
-        
+
+        boolean correcte = false;
+
+        ValidarSuperusuari(correcte);
     }
 
-    public static void ValidarSuperusuari() {
+    public static boolean ValidarSuperusuari(boolean correcte) {
         Scanner scan = new Scanner(System.in);
-        
-        final int numIntents = 3;
-        String contrasenya;
-        
-        for (int i = 0; i < numIntents; i++) {
-            System.out.println("Introdueix la contrasenya: ");
-            contrasenya = scan.nextLine();
-        }
+
+        final int numIntentos = 3;
+        int numError = 0;
+        String contraCorrecte = "DAM-M03";
+        String contrasenya = null;
+
+        do {
+            for (int i = 0; i < numIntentos; i++) {
+                System.out.println("Introdueix la contrasenya: ");
+                contrasenya = scan.nextLine();
+                if (contrasenya.equals(contraCorrecte)) {
+                    correcte = true;
+                    System.out.println("Contrasenya correcte. " + numError + " intents.");
+                } else {
+                    correcte = false;
+                    numError++;
+                    System.out.println("Incorrecte. Prova de nou.");
+                }
+            }
+        } while (correcte = false);
+
+        scan.close();
+
+        return correcte;
     }
 }
